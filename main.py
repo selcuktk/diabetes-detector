90e67902bdf93e5ac1576a16104c22a175d54b06
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-03-03 19:40:16 +0300

    Parameters initialized

--- main.py
+++ main.py
@@ -26,6 +26,30 @@ class Main:
         # Also it is non-modular and hardcoded.
 
 
+        # initialization of the parameters for the model
+
+
+        # initialization of the weights values
+        # in this part of the project, all the number of units for layers are respectively 8, 6, 5, 1
+        # 8 refers to number of features (X.shape[0])
+        
+        wL1 = np.random.randn(6, X.shape[0]) * 0.01      
+        wL2 = np.random.randn(5, 6) * 0.01
+        wL3 = np.random.randn(1, 5) * 0.01
+
+        # initialization of the bias values
+
+        bL1 = np.zeros((6, 1))
+        bL2 = np.zeros((5, 1))
+        bL3 = np.zeros((1, 1))
+
+        # cost value, learning rate, number of iterations in for loop respectively
+        
+        J = 0
+        learning_rate = 0.09
+        num_iterations = 10000
+
+
         
         
             
