b7e28774f14cccb563c92f0e534d9c80f047d36a
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-03-04 19:58:07 +0300

    compute_cost() added

--- dl_utils.py
+++ dl_utils.py
@@ -164,3 +164,24 @@ class dlf:
         assert(AL.shape == (1,X.shape[1]))
                 
         return AL, stores
+
+    @staticmethod
+    def compute_cost(AL, Y):
+        """
+        Arguments:
+        AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
+        Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+        Returns:
+        cost -- cross-entropy cost
+        """
+        
+        m = Y.shape[1]
+
+        # Compute loss from aL and y.
+        cost = (-1/m)*np.sum((Y * np.log(AL)) + ((1-Y) * np.log(1-AL)), axis=1)
+        
+        cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+        assert(cost.shape == ())
+        
+        return cost
