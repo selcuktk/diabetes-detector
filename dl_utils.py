34746ba5b8601ef0e7f94c5174dd7d0730a1193f
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-03-04 17:53:05 +0300

    linear_activation_forward() added

--- dl_utils.py
+++ dl_utils.py
@@ -99,3 +99,35 @@ class dlf:
         store = (A, W, b)
         
         return Z, store
+    
+    @staticmethod
+    def linear_activation_forward(A_prev, W, b, activation):
+        """
+        Implement the forward propagation for the LINEAR->ACTIVATION layer
+
+        Arguments:
+        A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+        W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+        b -- bias vector, numpy array of shape (size of the current layer, 1)
+        activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+        Returns:
+        A -- the output of the activation function, also called the post-activation value 
+        store -- a python dictionary containing "linear_store" and "activation_store";
+                stored for computing the backward pass efficiently
+        """
+        
+        if activation == "sigmoid":
+            # Inputs: "A_prev, W, b". Outputs: "A, activation_store".
+            Z, linear_store = dlf.linear_forward(A_prev, W, b) 
+            A, activation_store = dlf.sigmoid(Z)
+        
+        elif activation == "relu":
+            # Inputs: "A_prev, W, b". Outputs: "A, activation_store".
+            Z, linear_store = dlf.linear_forward(A_prev, W, b) 
+            A, activation_store = dlf.relu(Z) 
+        
+        assert (A.shape == (W.shape[0], A_prev.shape[1]))
+        store = (linear_store, activation_store)
+
+        return A, store
