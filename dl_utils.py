36635fd245275f3c856cef8f706a75520acdf8f1
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-03-04 13:21:55 +0300

    initialize_parameters() function added

--- dl_utils.py
+++ dl_utils.py
@@ -28,3 +28,17 @@ class activation:
         daL = np.ones_like(aL)  
         daL[aL <= 0] = 0       
         return daL
+    
+class dl_functions:
+    @staticmethod
+    def initialize_parameters(lsz):
+        # lsz is the list that contains size of the hidden layers and output layer
+        parameters = {}
+        L = len(lsz)
+        for l in range(1, L):
+            parameters["w" + str(l)] = np.random.randn(lsz[l], lsz[l-1]) * 0.01
+            parameters["b" + str(l)] = np.zeros((lsz[l], 1))
+
+            assert(np.shape(parameters["w" + str(l)]) == (lsz[l], lsz[l-1]))
+            assert(np.shape(parameters["b" + str(l)]) == (lsz[l], 1))
+        return parameters
