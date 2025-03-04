489d47a8dc99dbabea5aee9e356c1d8a8d5d4b71
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-03-04 22:35:50 +0300

    L_model_backward() added

--- dl_utils.py
+++ dl_utils.py
@@ -239,3 +239,45 @@ class dlf:
             dA_prev, dW, db = dlf.linear_backward(dZ, linear_store)  
         
         return dA_prev, dW, db
+    
+    @staticmethod
+    def L_model_backward(AL, Y, stores):
+        """
+        Implement the backward propagation for the [LINEAR->RELU] * (L-1) -> LINEAR -> SIGMOID group
+        
+        Arguments:
+        AL -- probability vector, output of the forward propagation (L_model_forward())
+        Y -- true "label" vector (containing 0 if non-cat, 1 if cat)
+        stores -- list of stores containing:
+                    every store of linear_activation_forward() with "relu" (it's stores[l], for l in range(L-1) i.e l = 0...L-2)
+                    the store of linear_activation_forward() with "sigmoid" (it's stores[L-1])
+        
+        Returns:
+        grads -- A dictionary with the gradients
+                grads["dA" + str(l)] = ... 
+                grads["dW" + str(l)] = ...
+                grads["db" + str(l)] = ... 
+        """
+        grads = {}
+        L = len(stores) # the number of layers
+        m = AL.shape[1]
+        Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
+        
+        # Initializing the backpropagation
+        dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL)) 
+        
+        # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "dAL, current_store". Outputs: "grads["dAL-1"], grads["dWL"], grads["dbL"]
+        current_store = stores[L-1]  
+        grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = dlf.linear_activation_backward(dAL, current_store, "sigmoid") 
+        
+        # Loop from l=L-2 to l=0
+        for l in reversed(range(L-1)):
+            # lth layer: (RELU -> LINEAR) gradients.
+            # Inputs: "grads["dA" + str(l + 1)], current_store". Outputs: "grads["dA" + str(l)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)] 
+            current_store = stores[l] 
+            dA_prev_temp, dW_temp, db_temp = dlf.linear_activation_backward(grads["dA"+str(l+1)], current_store, "relu")
+            grads["dA" + str(l)] = dA_prev_temp
+            grads["dW" + str(l + 1)] = dW_temp
+            grads["db" + str(l + 1)] = db_temp 
+
+        return grads
